ab43991e961e0bfa2b7a427eafaa290cb316c57a
Elena Petrova <153084185+rear-parched@users.noreply.github.com>
2023-12-08 16:49:06 +0700

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,30 @@
+// Customer.java
+public class Customer {
+    private int customerId;
+    private String name;
+    private String email;
+
+    public Customer(int customerId, String name, String email) {
+        this.customerId = customerId;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void displayInfo() {
+        System.out.println("Customer ID: " + customerId);
+        System.out.println("Name: " + name);
+        System.out.println("Email: " + email);
+    }
+}
